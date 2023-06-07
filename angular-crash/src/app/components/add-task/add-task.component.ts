1acc0ddfde0d907e4cd890a15d99976f48cda2f8
Ford Henley <fordp86@gmail.com>
2023-06-07 14:22:24 -0500

    Completed Project
    Added routes for home and about. Project is complete.

--- angular-crash/src/app/components/add-task/add-task.component.ts
+++ angular-crash/src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 import { Task } from '../../Task';
 
 @Component({
@@ -12,6 +14,12 @@ export class AddTaskComponent {
   text: string;
   day: string;
   reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value))
+  }
 
   onSubmit() {
     if (!this.text) {
